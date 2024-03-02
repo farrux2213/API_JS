8f2f75385c4f5f10dbb01fdea149c548fd6a584a
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-03-03 00:40:12 +0500

    all commit

--- index.js
+++ index.js
@@ -20,27 +20,27 @@
 
 // ENTER TO THE FACEBOOK BY PROMISE
 
-var pww = "wolf66";
-var email = "wolves660@gmail.com";
+// var pww = "wolf66";
+// var email = "wolves660@gmail.com";
 
-const promise = new Promise((resolve, reject) => {
-  console.log("Biroz kuting ...!");
-  setTimeout(() => {
-    if (pww === "wolf66" && email === "wolves660@gmail.com") {
-      resolve("Hush kelibsiz saytimizga!");
-    } else {
-      reject("Ooops somthing wrong..!");
-    }
-  }, 4000);
-});
+// const promise = new Promise((resolve, reject) => {
+//   console.log("Biroz kuting ...!");
+//   setTimeout(() => {
+//     if (pww === "wolf66" && email === "wolves660@gmail.com") {
+//       resolve("Hush kelibsiz saytimizga!");
+//     } else {
+//       reject("Ooops somthing wrong..!");
+//     }
+//   }, 4000);
+// });
 
-promise
-  .then((javob) => {
-    console.log(javob);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.log("Tabriklaymiz jamoamiz nomidan ...!");
-  });
+// promise
+//   .then((javob) => {
+//     console.log(javob);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log("Tabriklaymiz jamoamiz nomidan ...!");
+//   });
