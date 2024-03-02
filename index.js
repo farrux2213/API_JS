4bb6b3fedd001cb47c281d15acd531277f2d39ca
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-03-03 00:25:07 +0500

    commit all

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //ENTER TO THE FACEBOOK BY CALLBACK
 
-var pww = "wolf66";
-var email = "wolves660@gmail.com";
+// var pww = "wolf66";
+// var email = "wolves660@gmail.com";
 
-function enterFacebook(pww, email, callback) {
-  console.log("Mr or Mrs Waiting!");
-  setTimeout(() => {
-    if (pww === "wolf66" && email === "wolves660@gmail.com") {
-      return callback("Hush kelibsiz saytimizga!");
-    } else {
-      return callback("Qandaydur xatolik kuzatildi");
-    }
-  }, 4000);
-}
+// function enterFacebook(pww, email, callback) {
+//   console.log("Mr or Mrs Waiting!");
+//   setTimeout(() => {
+//     if (pww === "wolf66" && email === "wolves660@gmail.com") {
+//       return callback("Hush kelibsiz saytimizga!");
+//     } else {
+//       return callback("Qandaydur xatolik kuzatildi");
+//     }
+//   }, 4000);
+// }
 
-enterFacebook(pww, email, (javob) => {
-  console.log(javob);
-});
+// enterFacebook(pww, email, (javob) => {
+//   console.log(javob);
+// });
