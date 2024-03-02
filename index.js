87d6fc55cfc6f8d64421a2040c90cdb5a91c45c5
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-03-03 00:24:41 +0500

    facebokka kirish kodi

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("API");
+//ENTER TO THE FACEBOOK BY CALLBACK
+
+var pww = "wolf66";
+var email = "wolves660@gmail.com";
+
+function enterFacebook(pww, email, callback) {
+  console.log("Mr or Mrs Waiting!");
+  setTimeout(() => {
+    if (pww === "wolf66" && email === "wolves660@gmail.com") {
+      return callback("Hush kelibsiz saytimizga!");
+    } else {
+      return callback("Qandaydur xatolik kuzatildi");
+    }
+  }, 4000);
+}
+
+enterFacebook(pww, email, (javob) => {
+  console.log(javob);
+});
